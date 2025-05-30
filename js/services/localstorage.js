11cbeceb388868e3bc589bea2a563f7f571f1ac8
henrrycoronado <coronadovillcahenrryalberto@gmail.com>
2025-05-30 16:14:51 -0400

    task1-localstorage

---
+++ js/services/localstorage.js
@@ -0,0 +1,48 @@
+export default class LocalStorageService {
+    constructor() {
+        this.storage = window.localStorage;
+    }
+    save(key, data) {
+        try {
+            const serializedData = JSON.stringify(data);
+            this.storage.setItem(key, serializedData);
+            return true;
+        } catch (error) {
+            console.error('Error al guardar en el local storage:', error);
+            return false;
+        }
+    }
+    get(key) {
+        try {
+            const item = this.storage.getItem(key);
+            return item ? JSON.parse(item) : null;
+        } catch (error) {
+            console.error('Error al obtener en el local storage:', error);
+            return null;
+        }
+    }
+    remove(key) {
+        try {
+            this.storage.removeItem(key);
+            return true;
+        } catch (error) {
+            console.error('Error eliminando en el local storage:', error);
+            return false;
+        }
+    }
+    clear() {
+        try {
+            this.storage.clear();
+            return true;
+        } catch (error) {
+            console.error('Error limpiando la local storage:', error);
+            return false;
+        }
+    }
+    exists(key) {
+        return this.storage.getItem(key) !== null ? true : false;
+    }
+    getAllKeys() {
+        return Object.keys(this.storage);
+    }
+}
